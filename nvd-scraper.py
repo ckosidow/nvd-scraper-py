a5c3b2571387429649feefaae8399c566afd5862
Conor <ckosidowski11@winona.edu>
2017-05-10 12:33:06 -0500

    Outputting to file

--- nvd-scraper.py
+++ nvd-scraper.py
@@ -3,19 +3,25 @@ import requests
 import csv
 
 with open('dependencies.csv') as csvfile:
-    dependencyReader = csv.reader(csvfile)
+    with open('out.csv', 'w', newline='') as outFile:
+        vulnWriter = csv.writer(outFile)
+        dependencyReader = csv.reader(csvfile)
 
-    for row in dependencyReader:
-        query = "".join(row)
+        for row in dependencyReader:
+            query = "".join(row)
 
-        page = requests.get(
-            "https://nvd.nist.gov/vuln/search/results?adv_search=false&form_type=basic&results_type=overview&search_type=last3months&query=" + query)
-        tree = html.fromstring(page.content)
+            page = requests.get(
+                "https://nvd.nist.gov/vuln/search/results?adv_search=false&form_type=basic&results_type=overview&search_type=last3months&query=" + query)
+            tree = html.fromstring(page.content)
 
-        resultTable = tree.xpath('//table[@data-testid="vuln-results-table"]//tbody//tr//th//a/@href')
+            resultTable = tree.xpath('//table[@data-testid="vuln-results-table"]//tbody//tr//th//a/@href')
 
-        if resultTable:
-            for result in resultTable:
-                print(query + " - https://nvd.nist.gov" + result)
-        else:
-            print(query + " - nothing found")
+            if resultTable:
+                for result in resultTable:
+                    vulnWriter.writerow([query, 'https://nvd.nist.gov' + result])
+
+                    print(query + " - https://nvd.nist.gov" + result)
+            else:
+                vulnWriter.writerow([query])
+
+                print(query + " -")
